54578dc53753f90666dac12eb12129389d1b0570
Zidane <124874973+Zidaneravilimita@users.noreply.github.com>
2024-04-04 13:38:05 +0300

    First Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
     const btnFermer = document.querySelector('.btn-fermer-slider');
     const btnOuvrir = document.querySelector('.vr_pls1');
-   /* const btnOuvrir_2 = document.querySelector('.vr_pls2');*/
     const fenetreImage = document.querySelector('.agrandirImg1');
-    /*const fenetreImage2 = document.querySelector('.agrandirImg2');*/
+    const imgSlider = document.querySelector('.ImgSlider');
+    const prevBtn = document.querySelector('.prev1');
+    const nextBtn = document.querySelector('.next1');
+
+    let currentPosition = 0;
+    const images = document.querySelectorAll('.ImgSlider img');
+    const totalImages = images.length;
+   
 
     btnFermer.addEventListener('click', function() {
         fenetreImage.style.display = 'none';
     });
 
     btnOuvrir.addEventListener('click', function() {
-        fenetreImage.style.display = 'block'; // Réaffichage de la fenêtre
+        fenetreImage.style.display = 'block';
     });
 
-    /*Vr pl 2*/
-   /* btnOuvrir_2.addEventListener('click', function() {
-        fenetreImage2.style.display = 'block'; // Réaffichage de la fenêtre
-    });*/
+    prevBtn.addEventListener('click', slideLeft);
+    nextBtn.addEventListener('click', slideRight);
+
+    // Fonction pour déplacer le slider vers la gauche
+    function slideLeft() {
+        currentPosition -= 1;
+        if (currentPosition < 0) {
+            currentPosition = totalImages - 1;
+        }
+        imgSlider.style.transform = `translateX(-${currentPosition * 15}rem)`;
+    }
+
+    // Fonction pour déplacer le slider vers la droite
+    function slideRight() {
+        currentPosition += 1;
+        if (currentPosition >= totalImages) {
+            currentPosition = 0;
+        }
+        imgSlider.style.transform = `translateX(${currentPosition * 15}rem)`;
+    }
 });
